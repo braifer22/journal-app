a04729905cf788bc4d2e490e622111c1c64c34ad
Alexis Molina <braifer22@gmail.com>
2025-02-23 07:45:50 -0300

    feat: integrate Firestore and add journal slice to Redux store

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,5 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const FirebaseAuth = getAuth(app);
-
-console.log(FirebaseAuth.currentUser);
+export const FirebaseDB = getFirestore(app);
